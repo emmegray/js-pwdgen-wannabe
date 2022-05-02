719f1bf0f8282e92688cb20345ac6bbece45dd7c
Manuela Ienuso <lunaienuso@libero.it>
2022-05-02 10:01:35 +0200

    Added simple vrs psw in js and added style

--- script.js
+++ script.js
@@ -5,13 +5,22 @@
 4. write in the page "name lastname favcolor 22"
 */
 
+// Simple version
+const nome = "Pippo";
+const cognome = "Franchi"
+const colore = "Blu"
+const anni = 38;
+
+const password = `Simple Password Generator: ${nome}${cognome}${colore}${anni}${anni}`;
+
+document.getElementById("psw").innerHTML = password;
+
+// Random version with user input
 const firstName = prompt("Inserisci il nome");
 const lastName = prompt("Inserisci il cognome");
 const favColor = prompt("Inserisci il tuo colore preferito");
 const casualNumber = Math.ceil(Math.random() * 100);
 
-const outputStr = `
-Password Generator: ${firstName}${lastName}${favColor}${casualNumber}
-`;
+const outputStr = `Random Password Generator: ${firstName}${lastName}${favColor}${casualNumber}`;
 
 document.getElementById("output").innerHTML = outputStr;
